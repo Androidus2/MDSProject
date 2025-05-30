08c78279fd550d902bb3a55121f16918a823617f
daria-gabriela <daria-gabriela.vasile@s.unibuc.ro>
2025-05-30 10:27:23 +0300

    Test StrokeItem

--- QtPaintTest/Tester/tst_StrokeItem.cpp
+++ QtPaintTest/Tester/tst_StrokeItem.cpp
@@ -1,4 +1,4 @@
-#include "tst_StrokeItem.h"
+﻿#include "tst_StrokeItem.h"
 #include "StrokeItem.h"
 
 void StrokeItemTest::initTestCase() {
@@ -49,3 +49,59 @@ void StrokeItemTest::testClone() {
 
     delete cloned;
 }
+
+ void StrokeItemTest::testColorAndWidth() { 
+        QColor color(Qt::green);
+        qreal width = 2.5;
+        StrokeItem item(color, width);
+        QCOMPARE(item.color(), color);
+        QCOMPARE(item.width(), width);
+ }
+
+  void StrokeItemTest:: testOutlined() {  
+        StrokeItem item(Qt::black, 3.0);
+        item.setOutlined(true);
+        QVERIFY(item.isOutlined());
+        item.setOutlined(false);
+        QVERIFY(!item.isOutlined());
+    }
+
+  void StrokeItemTest::testCloneIndependence() {
+        StrokeItem original(Qt::blue, 5.0);
+        original.setOutlined(true);
+        StrokeItem* clone = original.clone();
+        QVERIFY(clone != nullptr);
+        QCOMPARE(clone->color(), original.color());
+        QCOMPARE(clone->width(), original.width());
+        QVERIFY(clone->isOutlined());
+        clone->setOutlined(false);
+        QVERIFY(!clone->isOutlined());
+        QVERIFY(original.isOutlined());
+        delete clone;
+    }
+
+        void StrokeItemTest::testOpacity() { 
+        StrokeItem item(Qt::red, 4.0);
+        item.setOpacity(0.5);
+        QCOMPARE(item.opacity(), 0.5);
+        item.setOpacity(1.0);
+        QCOMPARE(item.opacity(), 1.0);
+    }
+        void StrokeItemTest::testDefaultValues() {
+            StrokeItem item(QColor(Qt::black), 1.0);
+            QCOMPARE(item.color(), QColor(Qt::black));
+            QCOMPARE(item.width(), 1.0);
+            QVERIFY(!item.isOutlined());
+            QVERIFY(!item.isSelected());
+            QCOMPARE(item.opacity(), 1.0); // presupunând că opacitatea implicită e 1.0
+        }
+
+        void StrokeItemTest::testOpacityLimits() {
+            StrokeItem item(Qt::red, 2.0);
+            item.setOpacity(0.0);
+            QCOMPARE(item.opacity(), 0.0);
+            item.setOpacity(1.0);
+            QCOMPARE(item.opacity(), 1.0);
+        }
+
+   
